d107d3715d6aa5ef08fba90534a0ac3359e9f708
Mashonne <himashcareem16@gmail.com>
2023-09-02 00:27:31 +0530

    added post reqs too

---
+++ src/test/java/com/mashdev/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mashdev.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
